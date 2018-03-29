64de79853224568eebb2d034330245a8db3d6e11
Benjamin Lemasson <benjamin.lemasson@univ-grenoble-alpes.fr>
2018-03-29 11:27:09 +0200

    %debug the folder's architecture 		- Main folder 			-  Raw_data 			-  ROI_data 			-  Derived_data 			-  PSOM 			- ...

--- MIA2.m
+++ MIA2.m
@@ -1821,7 +1821,9 @@ else
         handles.database.Properties.UserData.MIA_Derived_data_path = [new_patient_directory, 'Derived_data', filesep];
         handles.database.Properties.UserData.PSOM_path = [new_patient_directory, 'PSOM', filesep];
         % update the path in the table
-        handles.database.Path(handles.database.Type == 'Scan') = handles.database.Properties.UserData.MIA_Raw_data_path;
+        %handles.database.Path(handles.database.Type == 'Scan') = handles.database.Properties.UserData.MIA_Raw_data_path;
+        handles.database.Path(handles.database.IsRaw == '0' & handles.database.Type == 'Scan',:) = handles.database.Properties.UserData.MIA_Derived_data_path ;
+        handles.database.Path(handles.database.IsRaw == '1' & handles.database.Type == 'Scan',:) = handles.database.Properties.UserData.MIA_Raw_data_path;
         handles.database.Path(handles.database.Type == 'ROI') = handles.database.Properties.UserData.MIA_ROI_path;
     end
 
@@ -8222,6 +8224,8 @@ function MIA_test_button_Callback(hObject, eventdata, handles)
 if ~isfield(handles, 'database')
     return
 end
+
+% handles.database.Path(handles.database.IsRaw == '0' & handles.database.Type == 'Scan',:) = [handles.database.Properties.UserData.MIA_data_path, 'Derived_data', filesep];
 
 % handles.database.Type(handles.database.SequenceName == 'Mask') = 'ROI';
 % guidata(hObject, handles)
