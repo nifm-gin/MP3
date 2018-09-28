479a1efb8e406727fdc35b0c94e153d53918e8e1
brosscle <clement.brossard@univ-grenoble-alpes.fr>
2018-09-29 00:35:43 +0200

    Module_DeltaR2 : Debug (add option NbEchoUsed).

--- MIA_pipeline/Module_DeltaR2.m
+++ MIA_pipeline/Module_DeltaR2.m
@@ -12,12 +12,13 @@ if isempty(opt)
     module_option(:,1)   = {'folder_out',''};
     module_option(:,2)   = {'flag_test',true};
     module_option(:,3)   = {'OutputSequenceName','AllName'};
-    module_option(:,4)   = {'RefInput',1};
-    module_option(:,5)   = {'InputToReshape',1};
-    module_option(:,6)   = {'Table_in', table()};
-    module_option(:,7)   = {'Table_out', table()};
-    module_option(:,8)   = {'output_filename_ext','DeltaR2'};
-    module_option(:,9)   = {'Output_orientation','First input'};
+    module_option(:,4)   = {'NbEchoUsed','3'};
+    module_option(:,5)   = {'RefInput',1};
+    module_option(:,6)   = {'InputToReshape',1};
+    module_option(:,7)   = {'Table_in', table()};
+    module_option(:,8)   = {'Table_out', table()};
+    module_option(:,9)   = {'output_filename_ext','DeltaR2'};
+    module_option(:,10)   = {'Output_orientation','First input'};
     
     
     
@@ -46,7 +47,8 @@ if isempty(opt)
     user_parameter(:,4)   = {'   .Output filename','char','DeltaR2','output_filename_ext','','',...
         {'Specify the name of the output scan.'
         'Default filename extension is ''DeltaR2''.'}'};
-    user_parameter(:,5)   = {'   .Output orientation','cell',{'First input', 'Second input'},'Output_orientation','','',...
+    user_parameter(:,5)   = {'   .Number of echo used','cell',{'1', '3'},'NbEchoUsed','', '',''};
+    user_parameter(:,6)   = {'   .Output orientation','cell',{'First input', 'Second input'},'Output_orientation','','',...
         {'Specify the output orientation'
         '--> Output orienation = First input'
         '--> Output orientation = Second input'
@@ -121,6 +123,9 @@ end
 input_pre = read_volume(input(1).nifti_header, input(ref_scan).nifti_header, 0);
 input_post = read_volume(input(2).nifti_header, input(ref_scan).nifti_header, 0);
 
+%input_pre=niftiread(files_in.In1{1});
+%input_post=niftiread(files_in.In2{1});
+
 %% WTF
 % %%% ? What's the difference between se_echotime and RAW_data_pre.echotime ?
 % %%% I don't think those informations are available in the nifti files.
@@ -132,81 +137,92 @@ input_post = read_volume(input(2).nifti_header, input(ref_scan).nifti_header, 0)
 % se_echo_pos = find(se_echo_pos == 1);
 %%
 %nb_echo_used=opt.nb_echos;
-se_echotime = J1.SpinEchoTime.value/1000; %Conversion from ms to sec
+se_echotime = J1.SpinEchoTime.value; 
 %se_echotime = J1.EchoTime.value;
 %se_echo_pos = 1:length(J1.EchoTime.value);
-[~,se_echo_pos] = min(abs(J1.EchoTime.value/1000 - se_echotime)); %Conversion from ms to sec
+[~,se_echo_pos] = min(abs(J1.EchoTime.value - se_echotime));
 %se_echo_pos = abs(J1.EchoTime.value/1000 - se_echotime) <= 1/1000; %Conversion from ms to sec
 %se_echo_pos = find(se_echo_pos == 1);
+NbEchoUsed = str2double(opt.NbEchoUsed);
 
-
-if size(input_pre,3) ~= size(input_post,3)
-    deltaR2_map=zeros(size(input_pre));
-    deltaR2_slice_nbr = 0;
-    for i = 1:size(input_pre, 3)
-        for j = 1:size(input_post, 3)
-            deltaR2_slice_nbr = deltaR2_slice_nbr+1;
-            % Compute the CMRO2 map each slice with the same offset
-            temp_avant=squeeze(input_pre(:,:,i,se_echo_pos));
-            temp_apres=squeeze(input_post(:,:,j,se_echo_pos));
-            index_avant=find(temp_avant<(1e-3*mean(temp_avant(:))));
-            index_apres=find(temp_apres<(1e-3*mean(temp_apres(:))));
-            
-            %To avoid division by small numbers
-            temp_apres(index_apres)=1;
-            warning off %#ok<WNOFF>
-            deltaR2=-(1/se_echotime) * log(temp_apres ./ temp_avant);%ms-1
-            warning on %#ok<WNON>
-            deltaR2(index_avant)=0;
-            deltaR2(index_apres)=0;
-            deltaR2_map(:,:,deltaR2_slice_nbr,1)=deltaR2;
-            
-        end
-    end
-    
-    if deltaR2_slice_nbr == 0
-        warning_text = sprintf('##$ Can not calculate the deltaR2 map because there is\n##$ no slice offset match between\n##$SO2map=%s\n##$ and \n##$CBFmap=%s',...
-            filename_SO2map,filename_CBF);
-        msgbox(warning_text, 'deltaR2 map warning') ;
-        return
-    end
-else
     % Compute the deltaR2 map for all slices
-    deltaR2_map.reco='';
-    deltaR2_map=zeros([size(input_pre,1), size(input_pre, 2), size(input_pre, 3)]);     % DeltaR2 map
-    for m_slice=1:size(input_pre, 3)       
-        temp_avant=squeeze(input_pre(:,:,m_slice,se_echo_pos));
-        temp_apres=squeeze(input_post(:,:,m_slice,se_echo_pos));
-        index_avant=find(temp_avant<(1e-3*mean(temp_avant(:))));
-        index_apres=find(temp_apres<(1e-3*mean(temp_apres(:)))); 
-        %To avoid division by small numbers
-        temp_apres(index_apres)=1;
-        warning off %#ok<WNOFF>
-        deltaR2=-(1/se_echotime) * log(temp_apres ./ temp_avant);%ms-1
+deltaR2_map=zeros([size(input_pre,1), size(input_pre, 2), size(input_pre, 3)]);     % DeltaR2 map
+for m_slice=1:size(input_pre, 3)
+    if NbEchoUsed == 1
+        temp_avant=squeeze(input_pre(:,:,m_slice,se_echo_pos),3);
+        temp_apres=squeeze(input_post(:,:,m_slice,se_echo_pos),3);
+    else
+        temp_avant=mean(squeeze(input_pre(:,:,m_slice,se_echo_pos-1:se_echo_pos+1)),3);
+        temp_apres=mean(squeeze(input_post(:,:,m_slice,se_echo_pos-1:se_echo_pos+1)),3);
+    end
+    index_avant=find(temp_avant<(1e-3*mean(temp_avant(:))));
+    index_apres=find(temp_apres<(1e-3*mean(temp_apres(:)))); 
+    %To avoid division by small numbers
+    temp_apres(index_apres)=1;
+    warning off %#ok<WNOFF>
+    deltaR2=-(1/se_echotime) * log(temp_apres ./ temp_avant);%ms-1
+
+%         deltaR2_slice_nbr = deltaR2_slice_nbr+1;
+%         % Compute the CMRO2 map each slice with the same offset
+%         if nb_echo_used == 1
+%             if rescale == 0
+%                 temp_avant=squeeze(RAW_data_pre.data(:,:,se_echo_pos,i));
+%                 temp_apres=squeeze(RAW_data_post.data(:,:,se_echo_pos,j));
+%             else
+%                 temp_avant=imresize(squeeze(RAW_data_pre.data(:,:,se_echo_pos,i)),[final_res final_res],'bilinear');
+%                 temp_apres=imresize(squeeze(RAW_data_post.data(:,:,se_echo_pos,j)),[final_res final_res],'bilinear');
+%             end
+%         else
+%             if rescale == 0
+%                 temp_avant=squeeze(mean(RAW_data_pre.data(:,:,se_echo_pos-1:se_echo_pos+1,i),3));
+%                 temp_apres=squeeze(mean(RAW_data_post.data(:,:,se_echo_pos-1:se_echo_pos+1,j),3));
+%             else
+%                 temp_avant=imresize(squeeze(mean(RAW_data_pre.data(:,:,se_echo_pos-1:se_echo_pos+1,i),3)),[final_res final_res],'bilinear');
+%                 temp_apres=imresize(squeeze(mean(RAW_data_post.data(:,:,se_echo_pos-1:se_echo_pos+1,j),3)),[final_res final_res],'bilinear');
+%             end
+%         end
+%         index_avant=find(temp_avant<(1e-3*mean(temp_avant(:))));
+%         index_apres=find(temp_apres<(1e-3*mean(temp_apres(:))));
+% 
+%         %To avoid division by small numbers
+%         temp_apres(index_apres)=1;
+%         warning off %#ok<WNOFF>
+%         deltaR2=-(1/se_echotime) * log(temp_apres ./ temp_avant);%ms-1
+%         warning on %#ok<WNON>
+%         deltaR2(index_avant)=0;
+%         deltaR2(index_apres)=0;
+%         deltaR2_map.reco.data(:,:,1,deltaR2_slice_nbr)=deltaR2;
+
+
+
+
+
+
+
+
 %         A = -(1./se_echotime)';
 %         B = log(temp_apres ./ temp_avant);
 %         B = permute(B, [3,2,1]);
 %         for i=1:size(B,3)
 %             deltaR2(i,:) = A*squeeze(B(:,:,i));
 %         end
-        %deltaR2= A.*B ;%ms-1
-        %deltaR2 = permute(deltaR2, [3,2,1]);
-        warning on %#ok<WNON>
-        %deltaR2(index_avant)=0;
-        %deltaR2(index_apres)=0;
-        deltaR2_map(:,:,m_slice,1)=deltaR2;
-        %Compute the err map
-        %proc.err(:,:,1,m_slice)=tmpim;
-    end
-    %Adapt the fov offsets and orientations infos
+    %deltaR2= A.*B ;%ms-1
+    %deltaR2 = permute(deltaR2, [3,2,1]);
+    warning on %#ok<WNON>
+    %deltaR2(index_avant)=0;
+    %deltaR2(index_apres)=0;
+    deltaR2_map(:,:,m_slice,1)=deltaR2;
+    %Compute the err map
+    %proc.err(:,:,1,m_slice)=tmpim;
 end
+
 
 
 % transform the OutputImages matrix in order to match to the nii header of the
 % first input (rotation/translation)
 
 OutputImages = deltaR2_map;
-OutputImages(OutputImages < 0) = -1;
+%OutputImages(OutputImages < 0) = -1;
 OutputImages(OutputImages > 5000) = -1;
 OutputImages(isnan(OutputImages)) = -1;
 if ~exist('OutputImages_reoriented', 'var')
