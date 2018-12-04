d949b12c284aaa4396d3bb12c1689ee78eeced64
Benjamin Lemasson <benjamin.lemasson@univ-grenoble-alpes.fr>
2018-12-04 13:36:21 +0100

    add user information

--- MIA_pipeline/Module_Smoothing.m
+++ MIA_pipeline/Module_Smoothing.m
@@ -2,44 +2,50 @@ function [files_in,files_out,opt] = Module_Smoothing(files_in,files_out,opt)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Initialization and syntax checks %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-%% Initialize the module's parameters with default values 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Initialize the module's parameters with default values
 if isempty(opt)
-
-%     %   % define every option needed to run this module
-%     % --> module_option(1,:) = field names
-%     % --> module_option(2,:) = defaults values
+    
+    %     %   % define every option needed to run this module
+    %     % --> module_option(1,:) = field names
+    %     % --> module_option(2,:) = defaults values
     module_option(:,1)   = {'folder_out',''};
     module_option(:,2)   = {'flag_test',true};
     module_option(:,3)   = {'output_filename_ext','_Smooth'};
     module_option(:,4)   = {'OutputSequenceName','Extension'};
     module_option(:,5)   = {'Type','2-D Gaussian'};
-    %module_option(:,6)   = {'HSize',3};
     module_option(:,6)   = {'Sigma',1};
     module_option(:,7)   = {'RefInput',1};
     module_option(:,8)   = {'InputToReshape',1};
     module_option(:,9)   = {'Table_in', table()};
     module_option(:,10)   = {'Table_out', table()};
     opt.Module_settings = psom_struct_defaults(struct(),module_option(1,:),module_option(2,:));
-% 
-        %% list of everything displayed to the user associated to their 'type'
-         % --> user_parameter(1,:) = user_parameter_list
-         % --> user_parameter(2,:) = user_parameter_type
-         % --> user_parameter(3,:) = parameter_default
-         % --> user_parameter(4,:) = psom_parameter_list
-         % --> user_parameter(5,:) = Scans_input_DOF : Degrees of Freedom for the user to choose the scan
-         % --> user_parameter(6,:) = IsInputMandatoryOrOptional : If none, the input is set as Optional. 
-         % --> user_parameter(7,:) = Help : text data which describe the parameter (it
-         % will be display to help the user)
-    user_parameter(:,1)   = {'Select one scan as input','1Scan','','',{'SequenceName'}, 'Mandatory',''};
-    user_parameter(:,2)   = {'Parameters','','','','', '', ''};
-    user_parameter(:,3)   = {'   .Output filename extension','char','_Smooth','output_filename_ext','', '',''};
-    user_parameter(:,4)   = {'   .Type','cell', {'2-D Gaussian', '3-D Gaussian'},'Type','', '',''};
-    %user_parameter(:,4)   = {'   .HSize','numeric',3,'HSize','', '',''};
-    user_parameter(:,5)   = {'   .Sigma','numeric',1,'Sigma','', '',''};
+    %
+    %% list of everything displayed to the user associated to their 'type'
+    % --> user_parameter(1,:) = user_parameter_list
+    % --> user_parameter(2,:) = user_parameter_type
+    % --> user_parameter(3,:) = parameter_default
+    % --> user_parameter(4,:) = psom_parameter_list
+    % --> user_parameter(5,:) = Scans_input_DOF : Degrees of Freedom for the user to choose the scan
+    % --> user_parameter(6,:) = IsInputMandatoryOrOptional : If none, the input is set as Optional.
+    % --> user_parameter(7,:) = Help : text data which describe the parameter (it
+    % will be display to help the user)
+    user_parameter(:,1)   = {'Description','Text','','','','',...
+        {
+        'This module is usefull if you would like to smooth an image'
+        ''
+        'User has to choose one method of smoothing and the sigma'
+        '    --> 2D'
+        '    --> 3D'
+        }'};
+    user_parameter(:,2)   = {'Select one scan as input','1Scan','','',{'SequenceName'}, 'Mandatory',''};
+    user_parameter(:,3)   = {'Parameters','','','','', '', ''};
+    user_parameter(:,4)   = {'   .Output filename extension','char','_Smooth','output_filename_ext','', '',''};
+    user_parameter(:,5)   = {'   .Type','cell', {'2-D Gaussian', '3-D Gaussian'},'Type','', '',''};
+    user_parameter(:,6)   = {'   .Sigma','numeric',1,'Sigma','', '',''};
     VariableNames = {'Names_Display', 'Type', 'Default', 'PSOM_Fields', 'Scans_Input_DOF', 'IsInputMandatoryOrOptional','Help'};
     opt.table = table(user_parameter(1,:)', user_parameter(2,:)', user_parameter(3,:)', user_parameter(4,:)', user_parameter(5,:)', user_parameter(6,:)', user_parameter(7,:)','VariableNames', VariableNames);
-%%
+    %%
     
     % So for no input file is selected and therefore no output
     % The output file will be generated automatically when the input file
@@ -47,14 +53,14 @@ if isempty(opt)
     files_in.In1 = {''};
     files_out.In1 = {''};
     return
-  
+    
 end
 %%%%%%%%
 
 
 if isempty(files_out)
     opt.Table_out = opt.Table_in;
-    opt.Table_out.IsRaw = categorical(0);   
+    opt.Table_out.IsRaw = categorical(0);
     opt.Table_out.Path = categorical(cellstr([opt.folder_out, filesep]));
     if strcmp(opt.OutputSequenceName, 'AllName')
         opt.Table_out.SequenceName = categorical(cellstr(opt.output_filename_ext));
@@ -78,15 +84,6 @@ if ~exist('files_in','var')||~exist('files_out','var')||~exist('opt','var')
     error('Smoothing:brick','Bad syntax, type ''help %s'' for more info.',mfilename)
 end
 
-% [path_nii,name_nii,ext_nii] = fileparts(char(files_in.In1{1}));
-% if ~strcmp(ext_nii, '.nii')
-%      error('First file need to be a .nii, not a %s. Path : %s, Name : %s, opt.files_in : %s', ext_nii, path_nii, ext_nii, files_in);  
-% end
-% 
-% %% Building default output names
-% if strcmp(opt.folder_out,'') % if the output folder is left empty, use the same folder as the input
-%     opt.folder_out = path_nii;    
-% end
 
 %% If the test flag is true, stop here !
 
@@ -107,12 +104,6 @@ N = niftiread(files_in.In1{1});
 info = niftiinfo(files_in.In1{1});
 [path, name, ~] = fileparts(files_in.In1{1});
 jsonfile = [path, '/', name, '.json'];
-% fid = fopen(jsonfile, 'r');
-% raw = fread(fid, inf, 'uint8=>char');
-% fclose(fid);
-% %raw = reshape(raw, 1,length(raw));
-% J = jsondecode(raw);
-
 J = ReadJson(jsonfile);
 
 Informations = whos('N');
@@ -133,7 +124,7 @@ end
 
 %h = fspecial(opt.Type, opt.HSize, opt.Sigma);
 Sigma = opt.Sigma;
-if strcmp(opt.Type, '2-D Gaussian') 
+if strcmp(opt.Type, '2-D Gaussian')
     for i=1:size(NewN,3)
         for j=1:size(NewN,4)
             FilteredImages(:,:,i,j) = imgaussfilt(NewN(:,:,i,j), Sigma);
@@ -159,7 +150,7 @@ info2.Filemoddate = char(datetime('now'));
 
 J = KeepModuleHistory(J, struct('files_in', files_in, 'files_out', files_out, 'opt', opt, 'ExecutionDate', datestr(datetime('now'))), mfilename);
 
-    
+
 niftiwrite(NewFilteredImages, files_out.In1{1}, info2)
 
 [path, name, ~] = fileparts(files_out.In1{1});
