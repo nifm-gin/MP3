48f4b21ec9d5c248c4032964de8037ebc053be22
MONTIGON <olivier.montigon@univ-grenoble-alpes.fr>
2019-04-04 14:22:12 +0200

    Update MRIFileManager.jar to 19.2.3a

---
+++ MRIFileManager/docs/buttontop.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
