ccc220d1f68c392b88b96f32b5f79081c7be2cf2
Aurelien <aurelien.delphin@univ-grenoble-alpes.fr>
2019-10-17 09:23:36 +0200

    Modified condition to avoid interpolation when not needed

--- MP3_pipeline/Modules_Repository/MRFingerprinting/Module_MRF_Rest_1D.m
+++ MP3_pipeline/Modules_Repository/MRFingerprinting/Module_MRF_Rest_1D.m
@@ -355,27 +355,31 @@ switch opt.combUsed
         if strcmp(opt.method, 'ClassicMRF') || ~exist(model_filename,'file')
             tmpPre = nan(size(Dico.MRSignals{1},1), length(Obs.EchoTime.value'));
             tmpPost = nan(size(Dico.MRSignals{1},1), length(Obs.EchoTime.value'));
-            if size(XobsPre,length(size(XobsPre))) ~= size(Dico.MRSignals{1},2)
+            if size(XobsPre,length(size(XobsPre))-1) ~= size(Dico.MRSignals{1},2)
                 warning('Sizes of scans and dictionary MR signals are differents: dictionary MR signals reshaped')
                 for i = 1:size(Dico.MRSignals{1},1)
                     tmpPre(i,:) = interp1(Dico.Tacq(1:size(Dico.MRSignals{1},2)), Dico.MRSignals{1}(i,:), Obs.EchoTime.value'*1e-3);
                     if strcmp(opt.indivNorm, 'Yes') % Normalize dico
                         tmpPre(i,:) = tmpPre(i,:)./(sqrt(sum(tmpPre(i,:).^2)));
-                    end
+                    end 
                     tmpPost(i,:) = interp1(Dico.Tacq(1:size(Dico.MRSignals{1},2)), Dico.MRSignals{2}(i,:), Obs.EchoTime.value'*1e-3);
                     if strcmp(opt.indivNorm, 'Yes') % Normalize dico
                         tmpPost(i,:) = tmpPost(i,:)./(sqrt(sum(tmpPost(i,:).^2)));
                     end
                 end
+                tmp = cat(2, tmpPre, tmpPost);
+            else
+                tmp = cat(2, Dico.MRSignals{1}, Dico.MRSignals{2});
             end
-            tmp                 = cat(2, tmpPre, tmpPost);
+            
             Dico.MRSignals      = tmp;
+            clear tmp;
             Dico.Tacq           = Obs.EchoTime.value'*1e-3;
             %remove row containning nan values
             nn                  = ~any(isnan(Dico.MRSignals),2);
             Dico.MRSignals      = Dico.MRSignals(nn,:);
             Dico.Parameters.Par = Dico.Parameters.Par(nn,:);
-%             Tmp{1}              = Dico;
+            %Tmp{1}              = Dico;
         end
         %end
         
