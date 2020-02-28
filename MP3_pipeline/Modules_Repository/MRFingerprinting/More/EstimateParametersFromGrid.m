ebe07e456280d42e3ab4a94b67aab9687d942536
Aurélien <aurelien.delphin@gmail.com>
2020-02-28 08:50:46 +0100

    Reduced RAM usage when matching

--- MP3_pipeline/Modules_Repository/MRFingerprinting/More/EstimateParametersFromGrid.m
+++ MP3_pipeline/Modules_Repository/MRFingerprinting/More/EstimateParametersFromGrid.m
@@ -4,76 +4,114 @@ narginchk(3, 5)
 if nargin == 4, flagNorm = 1; end
 if nargin == 3, verb = 0; flagNorm = 1; end
 
-if size(Xgrid,1) > 300000
+if flagNorm
+    Xobs = Xobs ./ vecnorm(Xobs,2,2);
+    Xgrid = Xgrid ./ vecnorm(Xgrid,2,2);
+else
+    Xobs = Xobs;
+    Xgrid = Xgrid;
+end
 
-    % normalization
+if size(Xgrid,1) > 300000
+    
     score = [];
     N = size(Xgrid,1);
-    chunk = 1e4;
+    chunk = 5e4;
     I = floor(N/chunk);
     best = zeros(size(Xobs,1),1);
     idx = ones(size(Xobs,1),1);
-    switch flagNorm
-        case 1
-            Xobs_normalized     = (1 ./ sum(Xobs.^2, 2) .^0.5) * ones(1, size(Xobs, 2))  .* Xobs;
-            Xgrid_normalized    = (1 ./ sum(Xgrid.^2, 2).^0.5) * ones(1, size(Xgrid, 2)) .* Xgrid;
-            % dot-product/scalar product comparison
-            for i = 1:I
-                Xgrid_chunk = Xgrid_normalized((i-1)*chunk +1 : i*chunk,:);
-                score       = Xobs_normalized * Xgrid_chunk';
-                %keep best result for this chunk
-                [localBest, localIdx] = max(score, [], 2);
-                idx(best<localBest) = localIdx(best<localBest)+(i-1)*chunk;
-                best(best<localBest)=localBest(best<localBest);
-
-            end
-            if mod(N,chunk)~=0
-                Xgrid_chunk =  Xgrid_normalized(i*chunk +1 : end,:);
-                score = Xobs_normalized * Xgrid_chunk';
-                %keep best result for this chunk
-                [localBest, localIdx] = max(score, [], 2);
-                idx(best<localBest) = localIdx(best<localBest)+(I-1)*chunk;
-                best(best<localBest)=localBest(best<localBest);
-
-            end
-
-        case 0
-            % dot-product/scalar product comparison
-            for i = 1:I
-                Xgrid_chunk = Xgrid((i-1)*chunk +1 : i*chunk,:);
-                score       = Xobs * Xgrid_chunk';
-                %keep best result for this chunk
-                [localBest, localIdx] = max(score, [], 2);
-                idx(best<localBest) = localIdx(best<localBest)+(i-1)*chunk;
-                best(best<localBest)=localBest(best<localBest);
-
-            end
-            if mod(N,chunk)~=0
-                Xgrid_chunk =  Xgrid(i*chunk +1 : end,:);
-                score = Xobs * Xgrid_chunk';
-                %keep best result for this chunk
-                [localBest, localIdx] = max(score, [], 2);
-                idx(best<localBest) = localIdx(best<localBest)+(I-1)*chunk;
-                best(best<localBest)=localBest(best<localBest);
-
-            end
-
-    %         score       = Xobs * Xgrid';
+    
+    
+    for i = 1:I
+%         Xgrid_chunk = Xgrid((i-1)*chunk +1 : i*chunk,:);
+%         scorre       = Xobs * Xgrid((i-1)*chunk +1 : i*chunk,:)';
+        %keep best result for this chunk
+%         [localBest, localIdx] = max(score, [], 2);
+        [localBest, localIdx] = max(Xobs * Xgrid((i-1)*chunk +1 : i*chunk,:)', [], 2);
+        idx(best<localBest) = localIdx(best<localBest)+(i-1)*chunk;
+        best(best<localBest)=localBest(best<localBest);
+        
     end
+    if mod(N,chunk)~=0
+%         Xgrid_chunk =  Xgrid(i*chunk +1 : end,:);
+%         score = Xobs * Xgrid(i*chunk +1 : end,:)';
+        %keep best result for this chunk
+%         [localBest, localIdx] = max(score, [], 2);
+        [localBest, localIdx] = max(Xobs*Xgrid(i*chunk +1 : end,:)', [], 2);
+        idx(best<localBest) = localIdx(best<localBest)+(I-1)*chunk;
+        best(best<localBest)=localBest(best<localBest);
+        
+    end
+    
+    
+    %     switch flagNorm
+    %         case 1
+    % %             Xobs     = (1 ./ sum(Xobs.^2, 2) .^0.5) * ones(1, size(Xobs, 2))  .* Xobs;
+    % %             Xgrid    = (1 ./ sum(Xgrid.^2, 2).^0.5) * ones(1, size(Xgrid, 2)) .* Xgrid;
+    %             Xobs = Xobs ./ vecnorm(Xobs,2,2);
+    %             Xgrid = Xgrid ./ vecnorm(Xgrid,2,2);
+    %             % dot-product/scalar product comparison
+    %             for i = 1:I
+    %                 Xgrid_chunk = Xgrid((i-1)*chunk +1 : i*chunk,:);
+    %                 score       = Xobs * Xgrid_chunk';
+    %                 %keep best result for this chunk
+    %                 [localBest, localIdx] = max(score, [], 2);
+    %                 idx(best<localBest) = localIdx(best<localBest)+(i-1)*chunk;
+    %                 best(best<localBest)=localBest(best<localBest);
+    %
+    %             end
+    %             if mod(N,chunk)~=0
+    %                 Xgrid_chunk =  Xgrid(i*chunk +1 : end,:);
+    %                 score = Xobs * Xgrid_chunk';%     switch flagNorm
+%         case 1
+%             Xobs     = (1 ./ sum(Xobs.^2, 2) .^0.5) * ones(1, size(Xobs, 2))  .* Xobs;
+%             Xgrid    = (1 ./ sum(Xgrid.^2, 2).^0.5) * ones(1, size(Xgrid, 2)) .* Xgrid;
+%             % dot-product/scalar product comparison
+
+    %                 %keep best result for this chunk
+    %                 [localBest, localIdx] = max(score, [], 2);
+    %                 idx(best<localBest) = localIdx(best<localBest)+(I-1)*chunk;
+    %                 best(best<localBest)=localBest(best<localBest);
+    %
+    %             end
+    %
+    %         case 0
+    %             % dot-product/scalar product comparison
+    %             for i = 1:I
+    %                 Xgrid_chunk = Xgrid((i-1)*chunk +1 : i*chunk,:);
+    %                 score       = Xobs * Xgrid_chunk';
+    %                 %keep best result for this chunk
+    %                 [localBest, localIdx] = max(score, [], 2);
+    %                 idx(best<localBest) = localIdx(best<localBest)+(i-1)*chunk;
+    %                 best(best<localBest)=localBest(best<localBest);
+    %
+    %             end
+    %             if mod(N,chunk)~=0
+    %                 Xgrid_chunk =  Xgrid(i*chunk +1 : end,:);
+    %                 score = Xobs * Xgrid_chunk';
+    %                 %keep best result for this chunk
+    %                 [localBest, localIdx] = max(score, [], 2);
+    %                 idx(best<localBest) = localIdx(best<localBest)+(I-1)*chunk;
+    %                 best(best<localBest)=localBest(best<localBest);
+    %
+    %             end
+    %
+    %     %         score       = Xobs * Xgrid';
+    %     end
 else
-% normalization
-    switch flagNorm
-        case 1
-            Xobs_normalized     = (1 ./ sum(Xobs.^2, 2) .^0.5) * ones(1, size(Xobs, 2))  .* Xobs;
-            Xgrid_normalized    = (1 ./ sum(Xgrid.^2, 2).^0.5) * ones(1, size(Xgrid, 2)) .* Xgrid;
+    % normalization
+%     switch flagNorm
+%         case 1
+%             Xobs     = (1 ./ sum(Xobs.^2, 2) .^0.5) * ones(1, size(Xobs, 2))  .* Xobs;
+%             Xgrid    = (1 ./ sum(Xgrid.^2, 2).^0.5) * ones(1, size(Xgrid, 2)) .* Xgrid;
+%             % dot-product/scalar product comparison
+%             score       = Xobs * Xgrid';
+%         case 0
             % dot-product/scalar product comparison
-            score       = Xobs_normalized * Xgrid_normalized';
-        case 0
-            % dot-product/scalar product comparison
-            score       = Xobs * Xgrid';
-    end
-
-    [best, idx]    = max(score, [], 2);
+%             score       = Xobs * Xgrid';
+%     end
+    
+    [best, idx]    = max(Xobs * Xgrid', [], 2);
 end
 
 
@@ -84,7 +122,7 @@ end
 % SSoo = sum( (Xobs  - mean(Xobs,  2)).^2, 2);
 % SSgg = sum( (Xgrid - mean(Xgrid, 2)).^2, 2);
 % SSog = (Xobs - mean(Xobs, 2)) * (Xgrid - mean(Xgrid, 2))';
-% 
+%
 % R2 = SSog.^2 ./ (SSoo * SSgg');
 % [~, idx]    = max(R2, [], 2);
 
