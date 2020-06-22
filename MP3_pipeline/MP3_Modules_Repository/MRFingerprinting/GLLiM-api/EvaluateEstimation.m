9b96d3a38227c8ace01bccdbc6e2170bb7a5c9c3
brosscle <clement.brossard@univ-grenoble-alpes.fr>
2020-06-22 16:56:21 +0200

    Most of the modules files moved to a specific git repository

--- MP3_pipeline/MP3_Modules_Repository/MRFingerprinting/GLLiM-api/EvaluateEstimation.m
+++
@@ -1,9 +0,0 @@
-function [Rmse, Nrmse, Mae, Nmae] = EvaluateEstimation(Ytrue, Yestim)
-
-Rmse    = nanmean( (Ytrue - Yestim).^2 ).^.5;
-Nrmse   = Rmse ./ nanmean( (Ytrue - nanmean(Ytrue)).^2 ).^.5;
-
-Mae     = nanmean( abs(Ytrue - Yestim) );
-Nmae    = Mae ./ nanmean( Ytrue - nanmean(Ytrue) );
-
-end
