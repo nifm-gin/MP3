8ba4d7963ebed76949d6f185daa7fe847f14c737
brosscle <clement.brossard@univ-grenoble-alpes.fr>
2020-06-25 17:48:19 +0200

    New version of the separation between furnished and downloadable modules

--- MP3_pipeline/MP3_pipeline.m
+++ MP3_pipeline/MP3_pipeline.m
@@ -61,25 +61,27 @@ handles.output = hObject;
 handles.MP3_data = varargin{3};
 Spl = strsplit(mfilename('fullpath'), filesep);
 %Spl{end} = ['Modules_Repository', filesep, '**', filesep, 'Module_*.m'];
-Spl{end} = 'MP3_Modules_Repository';
-list_mod = dir(strjoin(Spl, filesep));
-if length(list_mod)<3
-    Spl{end} = 'Modules_Furnished';
-    list_mod = dir(strjoin(Spl, filesep));
-end
+Spl{end} = 'Modules';
+path_modules = strjoin(Spl, filesep);
+list_mod = dir(path_modules);
+
 Mod_listing = {};
 for i=3:length(list_mod)
-    %name = '';
-    [~, nam, ext] = fileparts(list_mod(i).name);
-    if (~list_mod(i).isdir && ~strcmp(ext, '.m')) || (~list_mod(i).isdir && ~startsWith(nam, 'Module_')) || isempty(nam)% exist([list_mod(i).folder, filesep, list_mod(i).name])==
-        continue
-    end
     Mod_listing = [Mod_listing, list_mod(i).name];
-    if list_mod(i).isdir
-        %Mod_listing = [Mod_listing, list_mod(i).name];
-        SubMod = dir([list_mod(i).folder, filesep, list_mod(i).name, filesep, 'Module_*.m']);
-        for j=1:length(SubMod)
-            Mod_listing = [Mod_listing, ['   .', SubMod(j).name]];
+    list_bis = dir([path_modules, filesep, list_mod(i).name, filesep]);
+
+    for l=3:length(list_bis)
+        [~, nam, ext] = fileparts(list_bis(l).name);
+        if (~list_bis(l).isdir && ~strcmp(ext, '.m')) || (~list_bis(l).isdir && ~startsWith(nam, 'Module_')) || isempty(nam)% exist([list_mod(i).folder, filesep, list_mod(i).name])==
+            continue
+        end
+        Mod_listing = [Mod_listing, ['   .', list_bis(l).name]];
+        if list_bis(l).isdir
+            %Mod_listing = [Mod_listing, list_mod(i).name];
+            SubMod = dir([list_bis(l).folder, filesep, list_bis(l).name, filesep, 'Module_*.m']);
+            for j=1:length(SubMod)
+                Mod_listing = [Mod_listing, ['      --', SubMod(j).name]];
+            end
         end
     end
 end
