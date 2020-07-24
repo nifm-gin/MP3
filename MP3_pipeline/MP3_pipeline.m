632ad65d5b859badb2d8602419ddb1656313df29
benjamin lemasson <benjamin.lemasson@univ-grenoble-alpes.fr>
2020-07-24 10:21:11 +0200

    merge conflict dev --> new_type_Mfile

--- MP3_pipeline/MP3_pipeline.m
+++ MP3_pipeline/MP3_pipeline.m
@@ -1888,11 +1888,17 @@ if ~isfield(New_module.opt.Module_settings, 'AutomaticJobsCreation')  || ...
         %                         text = 'It seems that you are trying to create a module which takes as input a scan that exists both virtually and concretely. It it not clear which one you want to use. Please delete the concretely one from your database, filter your database, or modify the module that creates the virtualy one.';
         %                         warndlg(text)
         %                         return
+                            dat = Databtmp3;
                             Path = Databtmp3.Path(1);
                             Path = strsplit(char(Path), filesep);
                             Path{end-1} = 'Tmp';
                             Path = strjoin(Path, filesep);
                             Databtmp3 = Databtmp3(Databtmp3.Path == Path,:);
+                            %Now that we only selected the virtual scans,
+                            %lets select the real scans that dont have a
+                            %similar virtual scan.
+                            [~, ind_diff] = setdiff(RestrictedDatab, Databtmp3(:,~strcmp(FNames(1:end-3), 'Path')));
+                            Databtmp3 = [Databtmp3; dat(ind_diff,:)];
                         end
                     end
                         for l=1:size(Databtmp3,1)
@@ -1958,21 +1964,21 @@ if ~isfield(New_module.opt.Module_settings, 'AutomaticJobsCreation')  || ...
                 assert(size(NewIn,3) == size(RefMat,3));
                 FinalMat{InputToReshape} = NewIn;
             elseif size(InToReshape,1) == 1 && size(InToReshape,2) == 1 && EmptyParams{InputToReshape} == 1
-                string = ['Your ' Tag1 ' selection or your ' Tag2 ' selection is empty, but this multiple Tag is useless because it leads to a unique file. So, all the files from the input 2 will be coregistered on this unique file.'];
-                choice = questdlg(string, 'Unique file detected','Continue', 'Return', 'Return');
-                switch choice
-                    case 'Continue'
-                        NewIn = repmat(InToReshape, size(RefMat));
-                        assert(size(NewIn,1) == size(RefMat,1));
-                        assert(size(NewIn,2) == size(RefMat,2));
-                        assert(size(NewIn,3) == size(RefMat,3));
-                        FinalMat{InputToReshape} = NewIn;
-                    case 'Return'
-                        output_database = table();
-                        pipeline = struct();
-                        %set(handles.MP3_pipeline_manager_GUI, 'pointer', 'arrow');
-                        return
-                end
+                %string = ['Your ' Tag1 ' selection or your ' Tag2 ' selection is empty, but this multiple Tag is useless because it leads to a unique file. So, all the files from the input 2 will be coregistered on this unique file.'];
+                %                 choice = questdlg(string, 'Unique file detected','Continue', 'Return', 'Return');
+                %                 switch choice
+                %                     case 'Continue'
+                NewIn = repmat(InToReshape, size(RefMat));
+                assert(size(NewIn,1) == size(RefMat,1));
+                assert(size(NewIn,2) == size(RefMat,2));
+                assert(size(NewIn,3) == size(RefMat,3));
+                FinalMat{InputToReshape} = NewIn;
+                %                     case 'Return'
+                %                         output_database = table();
+                %                         pipeline = struct();
+                %                         %set(handles.MP3_pipeline_manager_GUI, 'pointer', 'arrow');
+                %                         return
+                %end
             elseif size(InToReshape,1) == 1 && EmptyParams{InputToReshape} == 1
                 NewIn = repmat(InToReshape, size(RefMat,1), 1);
                 assert(size(NewIn,1) == size(RefMat,1));
