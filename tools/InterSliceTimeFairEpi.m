93539dcda59df9c8bc6b442aa7419f6c2a141aaa
Benjamin Lemasson <benjamin.lemasson@univ-grenoble-alpes.fr>
2022-03-22 14:15:16 +0100

    debut CBF module in order to work with paravision 7

--- tools/InterSliceTimeFairEpi.m
+++ tools/InterSliceTimeFairEpi.m
@@ -1,16 +1,12 @@
 function interSliceTime = InterSliceTimeFairEpi(Json)
 %% Function for calculate interslice delay for FAIR labeling module with EPI acquisition module
-% wrtitten by C.Debacker & Lydiane Hirschler for PV6
+% wrtitten by C.Debacker & Lydiane Hirschler for PV6 and updated and cleaned by
+% Benjamin lemasson for PV7
 
-%         PVM_FovSatOnOff=scan_acqp('##$PVM_FovSatOnOff=',Map.texte,0);
-%PVM_FatSupOnOff=scan_acqp('##$PVM_FatSupOnOff=',Map.texte,0);
 PVM_FatSupOnOff = Json.FatSupOnOff.value{1};
 
-%PVM_FovSatModuleTime=scan_acqp('##$PVM_FovSatModuleTime=',Map.texte,1);
 PVM_FovSatModuleTime = Json.FovSatModuleTime.value;
-%PVM_FatSupModuleTime=scan_acqp('##$PVM_FatSupModuleTime=',Map.texte,1);
 PVM_FatSupModuleTime = Json.FatSupModuleTime.value;
-%SliceSpoilerDuration=scan_acqp('##$SliceSpoilerDuration=',Map.texte,1);
 if isfield(Json, 'SliceSpoilerDuration')
     SliceSpoilerDuration = Json.SliceSpoilerDuration.value;
 end
@@ -18,62 +14,44 @@ PVM_RampTime = 0.113;
 PVM_RiseTime = 0.114;
 PVM_GradDelayTime=0.001; % 1us
 CFG_AmplifierEnable = 0.05; % 50us
-%ExcPulse_Length= scan_acqp('##$ExcPulse=(',Map.texte,1);
 ExcPulse_Length = Json.ExcPulse.value{1};
 if ~isempty(ExcPulse_Length)
     ExcPulse_Length = strsplit(ExcPulse_Length, ', ');
     ExcPulse_Length = str2double(ExcPulse_Length{1});
 end
-%PVM_ExSliceRephaseTime=scan_acqp('##$PVM_ExSliceRephaseTime=',Map.texte,1);
 if isfield(Json, 'ExSliceRephaseTime_PVM')
     PVM_ExSliceRephaseTime = Json.ExSliceRephaseTime_PVM.value;
 end
-%PVM_ppgFlag1=scan_acqp('##$PVM_ppgFlag1=',Map.texte,0);
 PVM_ppgFlag1 = Json.ppgFlag1.value;
 PVM_ppgFlag1=char2logical(PVM_ppgFlag1);
-%EchoTime=scan_acqp('##$EchoTime=',Map.texte,1);
 EchoTime = Json.EchoTime.value;
 PVM_InterGradientWaitTime=0.01;
-%RefPulse_Length= scan_acqp('##$RefPulse=(',Map.texte,1);
 RefPulse_Length = Json.RefPulse.value{1};
 if ~isempty(RefPulse_Length)
     RefPulse_Length = strsplit(RefPulse_Length, ', ');
     RefPulse_Length = str2double(RefPulse_Length{1});
 end
-%SignalType=scan_acqp('##$SignalType=',Map.texte,0);
 SignalType = Json.SignalType.value{1};
-%PVM_MinEchoTime=scan_acqp('##$PVM_MinEchoTime=',Map.texte,1);
 PVM_MinEchoTime = Json.MinEchoTime.value;
-%PVM_EpiEchoDelay=scan_acqp('##$PVM_EpiEchoDelay=',Map.texte,1);
 PVM_EpiEchoDelay = Json.EpiEchoDelay.value;
-%PVM_EpiModuleTime=scan_acqp('##$PVM_EpiModuleTime=',Map.texte,1);
 PVM_EpiModuleTime = Json.EpiModuleTime.value;
-%PVM_TriggerModule=scan_acqp('##$PVM_TriggerModule=',Map.texte,0);
 PVM_TriggerModule = Json.TriggerModule.value{1};
 
-%PvVersion = scan_acqp('##TITLE=Parameter List, ParaVision ',Map.texte,0);
 PvVersion = Json.SoftwareVersions.value{1};
-if ~isnan(PvVersion(1)) && ~isempty(regexpi(PvVersion,'\w*6.\w*'))
-    %SliceSpoiler=strsplit(scan_acqp('##$SliceSpoiler=(',Map.texte,0),', ');
+if ~isnan(PvVersion(1)) && ~isempty(regexpi(PvVersion,'\w*6.\w*'))||~isempty(regexpi(PvVersion,'\w*7.\w*'))
     SliceSpoiler = strsplit(Json.SliceSpoiler.value{1}, ', ');
     SliceSpoilerDuration = str2double(SliceSpoiler(2));
-    %ExcPulse_Length= scan_acqp('##$ExcPulse1=(',Map.texte,1);
     ExcPulse_Length = Json.ExcPulse1.value{1};
     ExcPulse_Length = strsplit(ExcPulse_Length, ', ');
     ExcPulse_Length = str2double(ExcPulse_Length{1});
-    %PVM_ExSliceRephaseTime=scan_acqp('##$ExSliceRephaseTime=',Map.texte,1);
     PVM_ExSliceRephaseTime = Json.ExSliceRephaseTime.value;
-    %RefPulse_Length= scan_acqp('##$RefPulse1=(',Map.texte,1);
     RefPulse_Length = Json.RefPulse1.value{1};
     RefPulse_Length = strsplit(RefPulse_Length, ', ');
     RefPulse_Length = str2double(RefPulse_Length{1});
-    %SignalType=scan_acqp('##$PVM_SignalType=',Map.texte,0);
     SignalType = Json.SignalType.value{1};
 end
 if strcmp(PVM_TriggerModule,'On')
-    %PVM_TriggerMode=scan_acqp('##$PVM_TriggerMode=',Map.texte,0);
     PVM_TriggerMode = Json.TriggerMode.value{1};
-    %PVM_TriggerDelay=scan_acqp('##$PVM_TriggerDelay=',Map.texte,1);
     PVM_TriggerDelay = Json.TriggerDelay.value;
 else
     PVM_TriggerMode = 'empty';
@@ -81,11 +59,9 @@ end
 if strcmp(PVM_TriggerMode,'empty')
     PVM_TriggerModuleTime = 0;
 else
-    %TrigD0 = scan_acqp('##$PVM_TrigD0=',Map.texte,1);
     TrigD0 = Json.TrigD0.value;
     PVM_TriggerModuleTime = PVM_TriggerDelay + 0.04 + TrigD0;
     if isnan(PVM_TriggerModuleTime)
-       %PVM_TriggerModuleTime =  scan_acqp('##$PVM_TriggerModuleTime=',Map.texte,1);
        PVM_TriggerModuleTime = Json.TriggerModuleTime.value;
     end
     
